b5ffaedb48272c000acb321cb0d05b88f99139a1
Stephen <stefanodinucci@gmail.com>
2021-01-31 00:15:28 +0100

    Aggiungere i file di progetto.

---
+++ MoqyAPI/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoqyAPI.Models
+{
+    public class Cart
+    {
+        public int PurchaseID { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public int ProductID { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+    }
+}
---
+++ MoqyAPI/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoqyAPI.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+
+        public int PiecesLeft { get; set; }
+
+    }
+}
