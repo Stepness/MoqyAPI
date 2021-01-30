06d4c6f592ded4e460e485967095dfbcb208f5ba
Stephen <stefanodinucci@gmail.com>
2021-01-31 00:30:33 +0100

    Data and GetAll
    Insert some Data and GetAll

---
+++ MoqyAPI/Controllers/Shop.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MoqyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoqyAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Shop : ControllerBase
+    {
+        private readonly ILogger<Shop> _logger;
+
+        public Shop(ILogger<Shop> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetAll_PurchaseData")]
+        public IEnumerable<Cart> GetAll_PurchaseData()
+        {
+            return Cart.Carts;
+        }
+    }
+}
--- MoqyAPI/Models/Cart.cs
+++ MoqyAPI/Models/Cart.cs
@@ -14,5 +14,15 @@ namespace MoqyAPI.Models
         public int ProductID { get; set; }
 
         public DateTime PurchaseDate { get; set; }
+
+
+
+
+        public static List<Cart> Carts = new List<Cart>
+        {
+            new Cart() { PurchaseID = 1, CustomerID = 1, ProductID = 1, PurchaseDate = new DateTime(2019, 11, 2, 11, 00, 0)},
+            new Cart() { PurchaseID = 2, CustomerID = 2, ProductID = 2, PurchaseDate = new DateTime(2018, 10, 13, 15, 30, 0)},
+            new Cart() { PurchaseID = 3, CustomerID = 3, ProductID = 3, PurchaseDate = new DateTime(2020, 12, 3, 12, 30, 0)}
+        };
     }
 }
